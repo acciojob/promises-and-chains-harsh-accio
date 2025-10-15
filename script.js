9d0e143caad6dd74853b20a81b075e37e59265dc
harsh-accio <rajputharsh9897@gmail.com>
2025-10-15 09:53:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+const form = document.getElementById('form');
+form.addEventListener('submit',(event)=>{
+	event.preventDefault(); 
+	const age = document.getElementById('age').value.trim();
+	const name = document.getElementById('name').value.trim();
+
+	if(age=='' || name==''){
+		alert("Please enter valid details.")
+	}
+	else{
+		setTimeout(()=>{
+			if(Number(age)>18){
+				alert(`Welcome, ${name}. You can vote.`)
+			}
+			else{
+				alert(`Oh sorry ${name}. You aren't old enough.`)
+			}
+		},4000)
+	}
+})
